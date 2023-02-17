e9e74b3e60770724f8690d0cc31e411cdd04ff14
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-02-18 03:04:51 +0900

    refactor(Extension): 컬럼 설정 추가

--- src/main/java/com/example/extblock/domain/Extension.java
+++ src/main/java/com/example/extblock/domain/Extension.java
@@ -1,15 +1,33 @@
 package com.example.extblock.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
 import javax.persistence.*;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Extension {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column
+    @Column(nullable = false, length = 20)
     private String name;
     @Column
+    @ColumnDefault("false")
     private Boolean pin;
+
+    @Builder
+    public Extension(
+            String name,
+            Boolean pin
+    ) {
+        this.name = name;
+        this.pin = pin;
+    }
 }
---
+++ src/main/java/com/example/extblock/domain/ExtensionRepository.java
@@ -0,0 +1,6 @@
+package com.example.extblock.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExtensionRepository extends JpaRepository<Extension, Long> {
+}
