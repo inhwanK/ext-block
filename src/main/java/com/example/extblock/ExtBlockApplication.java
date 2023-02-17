bc5b1df4c38726c4806c8a3dfc24ad3e39cdcb5c
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-02-18 00:31:32 +0900

    Initial commit

---
+++ src/main/java/com/example/extblock/ExtBlockApplication.java
@@ -0,0 +1,13 @@
+package com.example.extblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtBlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtBlockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/extblock/ExtBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtBlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
