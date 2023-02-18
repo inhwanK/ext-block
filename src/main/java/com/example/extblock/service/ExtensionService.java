33c757ff8269c9484af61d0b310c800773d2c895
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-02-19 03:45:57 +0900

    chore(service): repository 주입

--- src/main/java/com/example/extblock/service/ExtensionService.java
+++ src/main/java/com/example/extblock/service/ExtensionService.java
@@ -1,7 +1,13 @@
 package com.example.extblock.service;
 
+import com.example.extblock.domain.ExtensionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class ExtensionService {
+
+    private final ExtensionRepository extensionRepository;
+
 }
