6b11aa012c84c471bf94f0d348a9bbec9fe1867e
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-02-20 15:52:43 +0900

    feat: 주요 api 추가
    커스텀 확장자 추가, 삭제 로직 구현

--- src/main/java/com/example/extblock/contoller/ExtensionController.java
+++ src/main/java/com/example/extblock/contoller/ExtensionController.java
@@ -8,6 +8,7 @@ import com.example.extblock.service.ExtensionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,21 +21,27 @@ public class ExtensionController {
     private final ExtensionRepository extensionRepository;
 
     // 고정 확장자 조회하기 - pin 된 것만
-    @GetMapping("/extension/pin")
+    @GetMapping("/extensions/pin")
     public List<ExtensionPinResponseDto> getPinExtensions() {
         List<Extension> extensions = extensionRepository.findByPinTrue();
         return pinResponseDtos(extensions);
     }
 
-    // 고정 확장자 생성하기 - pin 체크
-
-
     // 고정 확장자 check
+    @PatchMapping("/extensions/pin/valid")
+    public void checkPinExtension(@RequestParam String extensionName) {
+        return;
+    }
 
     // 고정 확장자 uncheck
+    @PatchMapping("/extensions/pin/invalid")
+    public void unCheckPinExtension(@RequestParam String extensionName) {
+        return;
+    }
+
 
     // 커스텀 확장자 조회하기
-    @GetMapping("/extension/custom")
+    @GetMapping("/extensions/custom")
     public List<ExtensionCustomResponseDto> getCustomExtensions() {
         List<Extension> extensions = extensionRepository.findByPinFalse();
         return customResponseDto(extensions);
@@ -53,15 +60,26 @@ public class ExtensionController {
     private List<ExtensionCustomResponseDto> customResponseDto(List<Extension> extensions) {
         List<ExtensionCustomResponseDto> dtos = new ArrayList<>();
         extensions.forEach(extension ->
-                dtos.add(new ExtensionCustomResponseDto(extension.getName()))
+                dtos.add(new ExtensionCustomResponseDto(extension.getExtensionName()))
         );
         return dtos;
     }
+
     // 커스텀 확장자 생성하기 - pin 되면 안됨
-    @PostMapping("/extension")
-    public void createCustomExtension(@RequestBody String name) {
+    @PostMapping("/extensions/custom")
+    public void createCustomExtension(
+            @RequestParam(name = "extensionName")
+            @NotBlank String extensionName
+    ) {
+        extensionService.createCustomExtension(extensionName);
+    }
+
+    // 커스텀 확장자 삭제하기 - pin 된 건 삭제하면 안됨
+    @DeleteMapping("/extensions/custom")
+    public void deleteCustomExtension(
+            @RequestParam
+            @NotBlank String extensionName
+    ) {
         return;
     }
-    // 커스텀 확장자 삭제하기 - pin 된 건 삭제하면 안됨
-
 }
--- src/main/java/com/example/extblock/service/ExtensionService.java
+++ src/main/java/com/example/extblock/service/ExtensionService.java
@@ -1,13 +1,58 @@
 package com.example.extblock.service;
 
+import com.example.extblock.domain.Extension;
 import com.example.extblock.domain.ExtensionRepository;
+import com.example.extblock.domain.vo.ExtensionName;
+import com.example.extblock.exception.InvalidExtensionException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ExtensionService {
 
     private final ExtensionRepository extensionRepository;
 
+    @Transactional
+    public void createCustomExtension(String extensionName) {
+        if (isPresentName(extensionName)) {
+            // 이미 존재하는 확장자 예외, Invalid 말고 다른걸로.
+            throw new InvalidExtensionException("이미 존재하는 확장자입니다.");
+        }
+
+        Extension extension = Extension.builder()
+                .extensionName(extensionName)
+                .checked(true)
+                .pin(false)
+                .build();
+
+        log.info("{}", extension);
+        extensionRepository.save(extension);
+    }
+
+    @Transactional
+    public void removeCustomExtension(String extensionName) {
+        if (!isCustomExtension(extensionName)) {
+            // 고정 확장자는 삭제할 수 없다는 예외
+            throw new InvalidExtensionException("고정 확장자는 삭제할 수 없습니다.");
+        }
+        extensionRepository.deleteByExtensionName(new ExtensionName(extensionName));
+    }
+
+    private Boolean isCustomExtension(String extensionName) {
+        if (!isPresentName(extensionName)) {
+            // 존재하지 않는 확장자 예외
+            throw new InvalidExtensionException("존재하지 않는 확장자입니다.");
+        }
+        return extensionRepository.findByExtensionName(new ExtensionName(extensionName)).get().getPin();
+    }
+
+    private Boolean isPresentName(String extensionName) {
+        return extensionRepository.findByExtensionName(new ExtensionName(extensionName))
+                .isPresent();
+    }
 }
